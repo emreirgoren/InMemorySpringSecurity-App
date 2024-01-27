d428363861b34e0f0b0ee3f4247aadb09f51a0ae
emreirgoren <emreirgoren@outlook.com>
2024-01-27 20:33:40 +0300

    Project Initialized

---
+++ src/main/java/com/inMemorySpringSecurity/InMemorySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.inMemorySpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemorySpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemorySpringSecurityApplication.class, args);
+	}
+
+}
